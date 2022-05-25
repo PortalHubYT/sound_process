b2f85e0ee0a29d3e6fdcb5d984a80f53645307d7
PortalHubYT <portalhub.skyblock@gmaiL.com>
2022-05-26 01:50:04 +0200

    more

---
+++ maps/__init__.py
@@ -0,0 +1,4 @@
+from .map1 import map as map_1
+from .map2 import map as map_2
+from .map3 import map as map_3
+from .map4 import map as map_4
--- peglin.py
+++ peglin.py
@@ -1,26 +1,33 @@
 import mcapi as mc
 import time
+import json
+from random import randrange
+from maps import *
 
 env = {
-    "width": 40,
-    "height": 30,
+    "width": 60,
+    "height": 50,
+    "depth": 2,
     "origin": mc.BlockCoordinates(0, 4, 0),
     "front_wall": mc.Block("barrier"),
-    "back_wall": mc.Block("obsidian"),
+    "back_wall": mc.Block("red_concrete"),
     "peg": mc.Block(
         "repeating_command_block",
         nbt=mc.NBT(
             {
-                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~-1 ~ ~ {"fuse":0}',
+                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~-1 ~-0.5 ~ {"fuse":0}',
                 "auto": 1,
             }
         ),
+    ),
+    "peg_tube": mc.Block(
+        "oak_trapdoor", blockstate=mc.BlockState({"facing": "west", "half": "top"})
     ),
     "left_bouncer": mc.Block(
         "repeating_command_block",
         nbt=mc.NBT(
             {
-                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~ ~ ~1 {"fuse":0}',
+                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~ ~ ~1.75 {"fuse":0}',
                 "auto": 1,
             }
         ),
@@ -29,7 +36,7 @@ env = {
         "repeating_command_block",
         nbt=mc.NBT(
             {
-                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~ ~ ~-1 {"fuse":0}',
+                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~ ~ ~-1.75 {"fuse":0}',
                 "auto": 1,
             }
         ),
@@ -38,50 +45,14 @@ env = {
         "repeating_command_block",
         nbt=mc.NBT(
             {
-                "Command": 'execute if entity @e[distance=..2] run summon minecraft:tnt ~ ~-1 ~ {"fuse":0}',
+                "Command": 'execute if entity @e[distance=..2.85, type=sheep] run summon minecraft:tnt ~ ~-1.65 ~ {"fuse":0}',
                 "auto": 1,
             }
         ),
     ),
-    "bouncer_walls": mc.Block("end_gateway"),
+    "bouncer_walls": mc.Block("slime_block"),
     "cover": mc.Block("bedrock"),
 }
-
-map_1 = """
-#####O####################################
-##########################################
-##########################################
-##########################################
-##########################################
-##########################################
-##########################################
-##########################################
-###O##################################O###
-##########################################
-##############################O###########
-###########O##############################
-###O##############################O#######
-##########################################
-#####O#####################O##############
-#################O###################O####
-#########O###############O################
-##############################O###########
-##########################################
-##########################################
-#####O#####################O##############
-#################O###################O####
-##########################################
-#########O###############O################
-##########################################
-##############################O###########
-#####O####################################
-###########################O##############
-####O#####O##########O###############O####
-##########################################
-##########################################
-#######O#####################O############
-##########################################
-"""
 
 
 def generate_board(clear=True, map=None):
@@ -94,56 +65,64 @@ def generate_board(clear=True, map=None):
     if map:
         untrimmed_grid = map.splitlines()
         grid = [x for x in untrimmed_grid if x]
+        x2 = x1 + env["depth"] - 1
         y2 = y1 + len(grid) - 1
         z2 = z1 + len(grid[0]) - 1
 
     else:
 
+        x2 = x1 + env["depth"] - 1
         y2 = y1 + env["height"] - 1
         z2 = z1 + env["width"] - 1
 
-    void = mc.Zone((x1, y1, z1), (x1, y1 - 4, z2))
+    void = mc.Zone((x1, y1, z1), (x2, y1 - 4, z2))
     mc.set_zone(void, mc.Block("air"))
 
     # Walls #
 
-    front_wall = mc.Zone((x1 - 1, y1, z1), (x1 - 1, y2, z2))
+    front_wall = mc.Zone((x1 - 1, y1 - 4, z1), (x1 - 1, y2, z2))
     mc.set_zone(front_wall, env["front_wall"])
 
-    back_wall = mc.Zone((x1 + 1, y1 - 4, z1), (x1 + 1, y2, z2))
+    back_wall = mc.Zone((x2 + 1, y1 - 4, z1), (x2 + 1, y2, z2))
+    mc.set_zone(back_wall, mc.Block("barrier"))
+
+    back_wall = mc.Zone((x2 + 2, y1 - 4, z1), (x2 + 2, y2, z2))
     mc.set_zone(back_wall, env["back_wall"])
 
     # Left bouncer #
 
-    left_bouncer = mc.Zone((x1, y1 - 4, z1 - 2), (x1, y2, z1 - 2))
-    mc.set_zone(left_bouncer, env["left_bouncer"])
+    for i in range(y1 - 4, y2):
+        pass
 
-    left_bouncer_wall = mc.Zone((x1, y1 - 4, z1 - 1), (x1, y2, z1 - 1))
+    left_bouncer = mc.Zone((x1, y1 - 4, z1 - 2), (x2, y2, z1 - 2))
+    # mc.set_zone(left_bouncer, env["left_bouncer"])
+
+    left_bouncer_wall = mc.Zone((x1, y1 - 4, z1 - 1), (x2, y2, z1 - 1))
     mc.set_zone(left_bouncer_wall, env["bouncer_walls"])
 
-    left_bouncer_cover = mc.Zone((x1 - 1, y1 - 4, z1 - 1), (x1 + 1, y2 + 3, z1 - 3))
+    left_bouncer_cover = mc.Zone((x1 - 1, y1 - 4, z1 - 1), (x2 + 1, y2 + 3, z1 - 3))
     mc.set_zone(left_bouncer_cover, env["cover"], handler="keep")
 
     # Right bouncer #
 
-    right_bouncer = mc.Zone((x1, y1 - 4, z2 + 2), (x1, y2, z2 + 2))
-    mc.set_zone(right_bouncer, env["right_bouncer"])
+    right_bouncer = mc.Zone((x1, y1 - 4, z2 + 2), (x2, y2, z2 + 2))
+    # mc.set_zone(right_bouncer, env["right_bouncer"])
 
-    right_bouncer_wall = mc.Zone((x1, y1 - 4, z2 + 1), (x1, y2, z2 + 1))
+    right_bouncer_wall = mc.Zone((x1, y1 - 4, z2 + 1), (x2, y2, z2 + 1))
     mc.set_zone(right_bouncer_wall, env["bouncer_walls"])
 
-    right_bouncer_cover = mc.Zone((x1 - 1, y1 - 4, z2 + 1), (x1 + 1, y2 + 3, z2 + 3))
+    right_bouncer_cover = mc.Zone((x1 - 1, y1 - 4, z2 + 1), (x2 + 1, y2 + 3, z2 + 3))
     mc.set_zone(right_bouncer_cover, env["cover"], handler="keep")
 
     # Cover
 
-    top_bouncer = mc.Zone((x1, y2 + 2, z1), (x1, y2 + 2, z2))
-    mc.set_zone(top_bouncer, env["top_bouncer"])
+    top_bouncer = mc.Zone((x1, y2 + 2, z1), (x2, y2 + 2, z2))
+    # mc.set_zone(top_bouncer, env["top_bouncer"])
 
-    top_bouncer_wall = mc.Zone((x1, y2 + 1, z1), (x1, y2 + 1, z2))
+    top_bouncer_wall = mc.Zone((x1, y2 + 1, z1), (x2, y2 + 1, z2))
     mc.set_zone(top_bouncer_wall, env["bouncer_walls"])
 
-    top_bouncer_cover = mc.Zone((x1 - 1, y2 + 1, z1 - 1), (x1 + 1, y2 + 3, z2 + 3))
+    top_bouncer_cover = mc.Zone((x1 - 1, y2 + 1, z1 - 1), (x2 + 1, y2 + 3, z2 + 3))
     mc.set_zone(top_bouncer_cover, env["cover"], handler="keep")
 
     # Bottom
@@ -160,17 +139,63 @@ def generate_board(clear=True, map=None):
                     pegs_coords.append((x, y))
 
         for coords in pegs_coords:
-            place_coords = (x1 + 1, y2 - coords[0], z1 + coords[1])
+            place_coords = (x2 + 1, y2 - coords[0], z1 + coords[1])
             mc.set_block(place_coords, env["peg"])
+
+            place_coords = (x2, y2 - coords[0] - 1, z1 + coords[1])
+            mc.set_block(place_coords, env["peg_tube"])
 
 
 def clear_map():
     x1, y1, z1 = env["origin"].set
-    clearance = mc.Zone((x1 - 3, y1, z1 - 4), (x1 + 3, 70, 60))
-    print(mc.set_zone(clearance, mc.Block("air")))
+
+    for y in range(125):
+        clearance = mc.Zone((x1 - 4, y1 + y, z1 - 3), (x1 + 4, y1 + y, 80))
+        mc.set_zone(clearance, mc.Block("air"))
+
+
+def spawn_random_balls(names, map=None):
+
+    x1, y1, z1 = env["origin"].set
+
+    if map:
+
+        untrimmed_grid = map.splitlines()
+        grid = [x for x in untrimmed_grid if x]
+        y2 = y1 + len(grid) - 1
+        z2 = z1 + len(grid[0]) - 1
+
+        rand_z = randrange(env["origin"].x, z2)
+
+    else:
+        y2 = y1 + env["height"] - 1
+        z2 = z1 + env["width"] - 1
+
+        rand_z = randrange(env["origin"].x, env["origin"].z + env["width"] - 1)
+
+    coords = mc.Coordinates(env["depth"] / 2, y2 - 0.5, rand_z)
+
+    command = (
+        f"summon slime {coords} "
+        + '{Size:1,CustomNameVisible:1b,CustomName:\'{"text":"'
+        + f"{names[randrange(len(names))]}"
+        + "\"}',ActiveEffects:[{Id:11b,Amplifier:5b,Duration:900000,ShowParticles:0b},{Id:28b,Amplifier:1b,Duration:9000,ShowParticles:0b}]}"
+    )
+
+    ret = mc.post(command)
 
 
 if __name__ == "__main__":
-    pass
+
+    with open("random_names_simplified.json", "r") as file:
+        names = json.load(file)
+
     mc.connect("localhost", "test")
-    generate_board(map=map_1, clear=True)
+
+    mc.post("kill @e[type=slime]")
+    mc.post("region flag __global__ tnt deny")
+    generate_board(map=map_3, clear=True)
+
+    while True:
+        spawn_random_balls(names, map=map_3)
+        time.sleep(0.5)
